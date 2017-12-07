2ec099b5236833def01f83a6088a2da34761dbbb
Fok <b.a.s.fok@student.tue.nl>
2017-12-07 12:15:25 +0100

    Initial commit

---
+++ controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -0,0 +1,77 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    AS_TitleBar_ba3d4433255e4ba2b7e1c138d727f1bd: function AS_TitleBar_ba3d4433255e4ba2b7e1c138d727f1bd(eventobject) {
+        var self = this;
+
+        function SHOW_ALERT__a524113769504409b0c1b5a80fb78499_True() {}
+        function SHOW_ALERT__a524113769504409b0c1b5a80fb78499_False() {}
+        function SHOW_ALERT__a524113769504409b0c1b5a80fb78499_Callback(response) {
+            if (response == true) {
+                SHOW_ALERT__a524113769504409b0c1b5a80fb78499_True()
+            } else {
+                SHOW_ALERT__a524113769504409b0c1b5a80fb78499_False()
+            };
+        }
+        kony.ui.Alert({
+            "alertType": constants.ALERT_TYPE_CONFIRMATION,
+            "alertTitle": "Light conditions",
+            "yesLabel": "OK.",
+            "noLabel": "Thanks!",
+            "message": "Light conditions succesfully measured!",
+            "alertHandler": SHOW_ALERT__a524113769504409b0c1b5a80fb78499_Callback
+        }, {
+            "iconPosition": constants.ALERT_ICON_POSITION_LEFT
+        })
+    },
+    AS_Button_b6cd26777ac2431686261a4fb5fdf6f7: function AS_Button_b6cd26777ac2431686261a4fb5fdf6f7(eventobject) {
+        var self = this;
+        if ((switchState == false)) {
+            self.view.ONoff2["text"] = "ON";
+            if (kony.theme.getCurrentTheme() != "default") {
+                kony.theme.setCurrentTheme("default", function() {
+                    self.view.ONoff2["skin"] = "ONskin";
+                }, null);
+            } else {
+                (function() {
+                    self.view.ONoff2["skin"] = "ONskin";
+                })();
+            }
+            if (kony.theme.getCurrentTheme() != "default") {
+                kony.theme.setCurrentTheme("default", function() {
+                    self.view.ONoff2["focusSkin"] = "FocusONskin";
+                }, null);
+            } else {
+                (function() {
+                    self.view.ONoff2["focusSkin"] = "FocusONskin";
+                })();
+            }
+        } else if ((switchState == true)) {
+            self.view.ONoff2["text"] = "OFF";
+            if (kony.theme.getCurrentTheme() != "default") {
+                kony.theme.setCurrentTheme("default", function() {
+                    self.view.ONoff2["skin"] = "OFFskin";
+                }, null);
+            } else {
+                (function() {
+                    self.view.ONoff2["skin"] = "OFFskin";
+                })();
+            }
+            if (kony.theme.getCurrentTheme() != "default") {
+                kony.theme.setCurrentTheme("default", function() {
+                    self.view.ONoff2["focusSkin"] = "FocusOFFskin";
+                }, null);
+            } else {
+                (function() {
+                    self.view.ONoff2["focusSkin"] = "FocusOFFskin";
+                })();
+            }
+        }
+        if (switchState == false) {
+            turnLightOn();
+        } else if (switchState == true) {
+            turnLightOff();
+        }
+    }
+});
---
+++ modules/Module.js
@@ -0,0 +1,15 @@
+var switchState = false;
+
+function turnLightOn(){
+  //Turn flashlight on
+  //alert("Flashlight turned on");
+  
+  switchState = true;
+}
+
+function turnLightOff(){
+  //Turn flashlight off
+  //alert("Flashlight turned off");
+  
+  switchState = false;
+}
